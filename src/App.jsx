d2ba23d99b3e9be82afea5df59d8547adb20e71e
田爪大智 <>
2024-12-07 10:09:45 +0900

    useStateの使い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 export const App = (props) => {
+  // stateを定義
+  const [num, setNum] = useState(0);
+
   // オブジェクトの分割代入
   // 初期値を持たせる
   const { color = 'black', children } = props;
@@ -9,14 +14,23 @@ export const App = (props) => {
     color,
   };
 
-  // イベント関数を作成
-  const alertEvent = () => alert('初めてのイベント');
+  // stateの更新イベント
+  // const plusNum = () => setNum(num + 1);
+
+  // Reactがstateをバッチ化していることを確認
+  const testPlusNum = () => {
+    // 1回しか実行されない(最後に実行されるstateの更新のみが行われる!!)
+    console.log('state更新');
+    setNum(num + 1);
+    setNum(num + 1); // これだけ実行される
+  };
 
   return (
     <>
       <h1 style={{ fontSize: 30, color: 'red' }}>Hello</h1>
       <h1 style={worldStyle}>{children}</h1>
-      <button onClick={alertEvent}>クリック</button>
+      <button onClick={testPlusNum}>クリック!</button>
+      <div style={{ textAlign: 'center', fontSize: 20 }}>{num}</div>
     </>
   );
 };
