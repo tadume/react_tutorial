ad1eba189221e2d351559c37baa7221ff05928a2
田爪大智 <>
2024-12-07 09:14:43 +0900

    Reactのインストールとフォーマッターの作成

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// index.htmlからID属性rootの要素をDOM操作で取得する
+const rootElement = document.getElementById('root');
+
+// 上記の要素をレンダリングすることで、JSとして扱えるようにする
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <>
+    {/* tsx または jsx にしないと、コンポーネントと判断されないので、自動インポートができない!!! */}
     <App />
-  </React.StrictMode>
+  </>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
