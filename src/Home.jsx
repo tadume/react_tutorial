d3ccec46e7286d9f260d31dbdf15cd83303ae466
田爪大智 <>
2024-12-07 17:40:46 +0900

    memo化することで、対象コンポーネントの不要な再レンダリングを抑制

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
-export const Page1 = () => {
+import { memo } from 'react';
+
+export const Page1 = memo(() => {
   console.log('-----Page1------');
 
   // 再レンダリングする必要のないコンポーネントなのに、App.jsxでstateを更新した際にこちらも更新されている。。
+  // メモ化したことで最初の一回しか再レンダリングされなくなった!!!!
   return <h2>Page1です。</h2>;
-};
+});
